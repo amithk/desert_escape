f4f2d46b4c60eb751d193c2577562e6af56284ef
Amit Kulkarni <amithkamithk@gmail.com>
2019-04-16 15:18:11 +0530

    Implement room door map

--- game_world.js
+++ game_world.js
@@ -18,13 +18,26 @@ class Building {
 	constructor() {
 		this.rooms = [];
 		this.totalRooms = 7;
+		this.doors = [];
+		this.totalDoors = 14;
 
 		// The rooms are not identical.
 		// Each rooms has a number of doors and some (not all) 
 		// of these doors have computers attached to them.
 		// Some of the rooms have some things.
 		for(i=0; i<this.totalRooms; i++) {
-			this.rooms[i] = new Room();
+			var map = getRoomDoorsBitmap(i)
+			this.rooms[i] = new Room(map);
+		}
+
+		// Doors are part of the building. Each door has a computer
+		// attached to it, from one of its side. The door can be
+		// opened only from the computer's side. Once opened, player
+		// can go from the either side to the other side.
+		for(i=0; i<this.totalDoors, i++) {
+			var frontRoom = getFrontRoom(i)
+			var backRoom  = getBackRoom(i)
+			this.doors[i] = newDoor(frontRoom, backRoom);
 		}
 	}
 }
@@ -40,11 +53,14 @@ class Room {
 }
 
 // The Door
+// Each door is a part of two rooms, front room and back room.
+// Front room has computer attached to the door inside this room.
+// Front/back room = -1 means it is the enter/exit to/from the building.
 class Door {
-}
-
-// The Computer
-class Computer {
+	constructor(frontRoom, backRoom) {
+		this.frontRoom = frontRoom;
+		this.backRoom  = backRoom;
+	}
 }
 
 // The Player
@@ -62,12 +78,24 @@ var gameWorld;
 
 function initGameWorld() {
 	initRoomDoorsBitmap();
+	initDoorFrontRoomMap();
+	initDoorBackRoomMap();
 	gameWorld = new GameWorld();
 }
 
 var roomDoorsBitmap = [];
+var doorFrontRoomMap = [];
+var doorBackRoomMap = [];
 
-function initRootDoorsBitmap() {
+function initDoorFrontRoomMap() {
+	doorFrontRoomMap[0] = [-1, 0, 6, 5, 1, 1, 2, 6, 3, 3, 3, 4, 6, 5];
+}
+
+function initDoorBackRoomMap() {
+	doorBackRoomMap[0] = [0, 1, 0, 0, 2, 6, 3, 2, 6, -1, 4, 5, 4, 6];
+}
+
+function initRoomDoorsBitmap() {
 	roomDoorsBitmap[0] = parseInt('111100', 2);
 	roomDoorsBitmap[1] = parseInt('111000', 2);
 	roomDoorsBitmap[2] = parseInt('110001', 2);
@@ -81,4 +109,11 @@ function getRoomDoorsBitmap(roomNo) {
 	return roomDoorsBitmap[roomNo];
 }
 
+function getFrontRoom(doorNo) {
+	return doorFrontRoomMap[doorNo];
+}
+
+function getBackRoom(doorNo) {
+	return doorBackRoomMap[doorNo];
+}
 
